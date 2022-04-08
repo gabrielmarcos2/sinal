b2c751a4962b05ed540da4ed55ed92e695ee2c6a
gabrielmarco2 <gabrielsqueda123@gmail.com>
2022-04-08 19:45:41 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int numero;
+
+Console.Write("digite um numero ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero < 0)
+{
+    Console.WriteLine("Negativo");
+}
+else if (numero > 0)
+{
+    Console.WriteLine("positivo");
+}
+else
+{
+    Console.WriteLine("zero");
+}
